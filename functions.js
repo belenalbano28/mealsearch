ca724bc7fa7767218a7e04a59080de9a7e1cea53
belencita28 <belenalristi@gmail.com>
2023-08-26 21:53:53 -0300

    arreglo

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
 
 $( document ).ready(function() {
     letter=generateRandomLetter();
+    recetasletra(letter);
+    $('#alert').css('display','none');
+});
+
+function recetasletra(letter){
+    
     url="https://www.themealdb.com/api/json/v1/1/search.php?f="+letter
     fetch(url).then(function(response) {
        return response.json();
@@ -10,6 +16,7 @@ $( document ).ready(function() {
        console.log(array);
        var y;
        var recetas=document.getElementById('recetas');
+       recetas.innerHTML='';
        
        for (let index = 0; index < array[0].meals.length; index++) {
         var z=$('#recetas').html();
@@ -17,24 +24,25 @@ $( document ).ready(function() {
         var nombre=array[0].meals[index].strMeal;
         var foto=array[0].meals[index].strMealThumb;
         var video=array[0].meals[index].strYoutube;
-        console.log(array[0].meals.length);
         if(index%3==0){
             y=index;
             //primera vez,multiplo de 3. 0,3,6,9
             var x='';
            x='<div class="row people"><div class="col-md-6 col-lg-4 item" onclick="pasardatos('+idMeal+')" ><div class="box"><img class="rounded-circle" src="'+foto+'"><h3 class="name">'+nombre+'</h3><a class="title" href="'+video+'">Youtube Video here</a></div></div>';
+           if(index==array[0].meals.length-1){
+            recetas.innerHTML = z+x+'</div>';
+            console.log('1');
+         }
         }else if(index==y+1){
             //segundo item 
             x+='<div class="col-md-6 col-lg-4 item" onclick="pasardatos('+idMeal+')" ><div class="box"><img class="rounded-circle" src="'+foto+'"><h3 class="name">'+nombre+'</h3><a class="title" href="'+video+'">Youtube Video here</a></div></div>';
-            if(index==array.length){
-                recetas.innerHTML = x+'</div>' +z;
+            if(index==array[0].meals.length-1){
+                recetas.innerHTML = z+x+'</div>' ;
+                console.log('2p');
             }
         }else if(index==y+2){
             //el ultimo item
             x+='<div class="col-md-6 col-lg-4 item" onclick="pasardatos('+idMeal+')" ><div class="box"><img class="rounded-circle" src="'+foto+'"><h3 class="name">'+nombre+'</h3><a class="title" href="'+video+'">Youtube Video here</a></div></div></div>';
-            if(index==array.length){
-                recetas.innerHTML = x+'</div>'+z;
-            }
             recetas.innerHTML = x+z;
         }
 
@@ -47,8 +55,7 @@ $( document ).ready(function() {
       }).catch(function(err) {
         console.log('Fetch Error :-S', err);
       });
-});
-
+}
 
 function generateRandomLetter() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -59,3 +66,22 @@ function generateRandomLetter() {
   function pasardatos(idMeal){
     console.log(idMeal);
   }
+
+  function search(){
+    x=document.getElementById('buscar').value;
+    if(x.length==1){
+        recetasletra(x);
+    }else{
+         var val=x.indexOf(' ');
+        if(val==-1){ 
+          recetasnombre(x); 
+        }else{
+            setTimeout(function(){
+                $('#alert').css('display','none'); 
+            }, 6000);
+            $('#alert').css('display','block');
+                $('#alert').html('Only one word for names! Try again.');
+        }
+    }
+  }
+
